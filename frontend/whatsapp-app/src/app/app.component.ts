fc45af36d659e5c9f3577a27f9ebc31b3c71ce3a
Nicolas Falla Rios <nicolasfalla15@gmail.com>
2024-08-10 13:05:04 -0500

    feat(2034): adsasd

--- frontend/whatsapp-app/src/app/app.component.ts
+++ frontend/whatsapp-app/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QrCodeModule } from 'ng-qrcode';
-import { WebSocketService } from './services/web-socket.service';
-import { Socket } from 'socket.io-client';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Socket, io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -13,99 +11,23 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'whatsapp-app';
+  private socket: Socket;
+  title = "funciona por favor :(";
 
-  qrCode: string | undefined = undefined;
-  status: string | undefined = undefined;
-
-  messages: string[] = [];
-
-  private errorSubject: Subject<string>;
-  errors$: Observable<string> | undefined;
-
-  private connectionSubject: Subject<boolean>;
-
-
-
-
-  constructor(private socketService: WebSocketService) {
-
-    this.errorSubject = new Subject();
-
-    this.connectionSubject = new Subject<boolean>
-    
-
-
-      // in this component you define what to do
-      // with received event of type 'message'
-      this.socketService.socket.on('message', (data: any) => {
-        this.messages.push(data);
-        console.log(this.messages)
-      });
-
-        // in this component you define what to do
-    // with received event of type 'message'
-    this.socketService.socket.on('message', (data: any) => {
-      this.messages.push(data);
-    });
-
-    // when receiving a 'connect' event, send a message to server
-    this.socketService.socket.on('connect', () => {
-      const message = 'myevent';
-      this.socketService.socket.emit(message, { data: "I'm connected!" });
-    });
+  constructor() {
+    this.socket = io('http://localhost:3000');
   }
 
+  ngOnInit() {
+    // Connect to the backend server
 
-  sendMessage(message: string): void {
-  }
-  getMessages() {
-
-  }
-
-
-
-
-  ngOnInit(): void {
-
-    this.socketService.getQr().subscribe(hello => {
-      console.log(hello);
-    })
-
-
-    this.monitorConnection().subscribe(it => {
-      console.log(it);
-    })
-
-
-
-    
-
- 
-  
-
-  }
-
-
-  private monitorConnection(): Observable<boolean> {
-    this.connectionSubject = new BehaviorSubject<boolean>(false);
-
-    this.socketService.socket.on('connect', () => {
-      this.connectionSubject.next(true);
+    // Handle events received from the server
+    this.socket.on('event-1', (data) => {
+      console.log('Received event-1:', data);
     });
 
-    this.socketService.socket.on('connection', () => {
-      this.connectionSubject.next(true);
-    });
-
-    this.socketService.socket.on('disconnect', () => {
-      this.connectionSubject.next(false);
-    });
-
-    this.socketService.socket.on('disconnecting', () => {
-      this.connectionSubject.next(false);
-    });
-
-    return this.connectionSubject.asObservable();
+    // Emit events to the server
+    this.socket.emit('event2', 'Hello from the client!');
   }
 }
+
